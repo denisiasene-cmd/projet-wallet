1fed5dec703b19c64ea5f829f550fccf197db302
denise <denisiasene2003@gmail.com>
2026-06-28 17:26:12 +0000

    feat: implementation de la creation de wallet avec blindages de saisie et prefixes

--- controller.php
+++ controller.php
@@ -2,33 +2,94 @@
 require_once "repository.php";
 require_once "validator.php";
 
-function routerAction($choix) {
-    $choix = trim($choix);
+function executerCreerWallet() {
+    echo "\n--- FORMULAIRE DE CRÉATION DE WALLET ---\n";
 
-    switch ($choix) {
+  
+    do {
+        $nom = readline("Entrez le nom du client : ");
+        if (!verifierFormatNom($nom)) {
+            echo "Erreur: Le nom est obligatoire .\n";
+        }
+    } while (!verifierFormatNom($nom));
+
+    do {
+        $telephone = readline("Entrez le numéro de téléphone : ");
+        $telephone = trim($telephone);
+        $telephoneValide = true;
+
+        if (!verifierChampObligatoire($telephone)) {
+            echo "Erreur: Le numéro de téléphone est obligatoire .\n";
+            $telephoneValide = false;
+        } elseif (!verifierEstNumerique($telephone) || !verifierTailleExacte($telephone, 9)) {
+            echo "Erreur: Le numéro doit comporter exactement 9 chiffres.\n";
+            $telephoneValide = false;
+        } elseif (!verifierPrefixeSenegal($telephone)) {
+            echo "Erreur: Préfixe invalide .\n";
+            $telephoneValide = false;
+        } elseif (!verifierUniciteTelephone($telephone)) {
+            echo "Erreur: Ce numéro de téléphone est déjà utilisé .\n";
+            $telephoneValide = false;
+        }
+    } while (!$telephoneValide);
+
+   
+    do {
+        $solde = readline("Entrez le solde initial (positif ou nul >= 0) : ");
+        $solde = trim($solde);
+        if (!verifierSoldeInitial($solde)) {
+            echo "Erreur: Le solde est obligatoire et doit être positif ou nul (RG1).\n";
+        }
+    } while (!verifierSoldeInitial($solde));
+
+    do {
+        $code = readline("Définissez un code secret (4 chiffres) : ");
+        $code = trim($code);
+        $codeValide = true;
+
+        if (!verifierChampObligatoire($code)) {
+            echo "Erreur: Le code secret est obligatoire .\n";
+            $codeValide = false;
+        } elseif (!verifierEstNumerique($code) || !verifierTailleExacte($code, 4)) {
+            echo "Erreur: Le code invalide.\n";
+            $codeValide = false;
+        } elseif (!verifierUniciteCode($code)) {
+            echo "Erreur: Ce code secret est déjà attribué à un autre compte (\n";
+            $codeValide = false;
+        }
+    } while (!$codeValide);
+
+    $nouveauWallet = [
+        'telephone' => $telephone,
+        'nom' => strtoupper($nom),
+        'solde' => (float)$solde,
+        'code' => $code
+    ];
+
+
+    ajouterWallet($nouveauWallet);
+    echo "\nSuccès: Le Wallet a été créé avec succès pour " . strtoupper($nom) . " !\n";
+}
+
+function routerAction($choix) {
+    switch (trim($choix)) {
         case '1':
-            echo "\n  Creer un wallet\n";
-           
+            executerCreerWallet();
             break;
-            
         case '2':
-            echo "\n faire dépôt...\n";
+            echo "\n  faire depots.\n";
             break;
-            
         case '3':
-            echo "\n faire retrait...\n";
+            echo "\n faire un retrait \n";
             break;
-            
         case '4':
-            echo "\n l'historique des transactions...\n";
+            echo "\n listes des trasactions\n";
             break;
-            
         case '0':
-            echo "\n Quitter\n";
+            echo "\n quitter \n";
             break;
-            
         default:
-            echo "\nVeuillez entrer un chiffre entre 0 et 4.\n";
+            echo "\nChoix invalide, veuillez réessayer.\n";
             break;
     }
 }
